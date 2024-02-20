b22d1dff5ffd6cd2472dcb1ca66ee5c722fd9e20
christineyamin <shweyaminoo159932@gmail.com>
2024-02-20 12:45:02 +0800

    add pratical2

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -1,16 +1,4 @@
-# total_price = 0
-# user_input = int(input("Numbers of items :"))
-# while user_input < 0:
-#     print("Invalid number of item.")
-#     user_input = int(input("Numbers of items :"))
-# for i in range (0,user_input+1):
-#     price = float(input("Price of the item :"))
-#     total_price += price
-#     if total_price >100 :
-#         discount_price = total_price * 0.10
-#         total_price=total_price-discount_price
-#         print(f"Total price for {user_input} items is $ {total_price}")
-# print(f"Total price for {user_input} items is $ {total_price}")
+
 total_price = 0
 user_input= int(input("Numbers of items : "))
 for i in range(0,user_input):
