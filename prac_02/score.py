6d2d81963acd287a8454493d7a7c89c79047f00b
christineyamin <shweyaminoo159932@gmail.com>
2024-02-25 16:09:44 +0800

    determin score

--- prac_02/score.py
+++ prac_02/score.py
@@ -5,8 +5,8 @@ def main():
     score= float(input("Enter score :"))
     determine_score(score)
     print(f"Your score {score} is {determine_score(score)}.")
-    random_score=random.randint(score,2)
-    print(random_score)
+    random_score=random.randint(0,100)
+    print(f"Random score {random_score} is {determine_score(random_score)}.")
 
 
 def determine_score(score):
