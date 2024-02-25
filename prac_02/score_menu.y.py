71751562cab1db769b37895fe6c1ad7882200cf1
christineyamin <shweyaminoo159932@gmail.com>
2024-02-25 17:10:55 +0800

    score menu

--- prac_02/score_menu.y.py
+++ prac_02/score_menu.y.py
@@ -1,15 +1,72 @@
+# menu= "(G)et a valid score \n(P)rint result\n(S)how stars\n(Q)uit"
+# print(menu)
+# choice=input(">>>").upper()
+# while choice!="Q":
+#     if choice=="G":
+#         user_score=float(input("Score :"))
+#         while user_score<0 or user_score>100:
+#             print("Invalid Score")
+#             user_score=float(input("Score :"))
+#         print(f"Your score is {user_score}")
+#     elif choice=="P":
+#         if user_score < 50:
+#             print("Bad")
+#         elif user_score > 90:
+#             print("Excellent")
+#         else:
+#             print("Passable")
+#     elif choice=="S":
+#         print("*" * len(user_score))
+#
+#     else:
+#         print("Invalid message")
+#     print(menu)
+#     choice=input(">>>").upper()
+#
+# print("Fare well.")
+#
+
+
 menu= "(G)et a valid score \n(P)rint result\n(S)how stars\n(Q)uit"
-print(menu)
-choice= input(">>>").upper()
-while choice!="Q":
-    if choice=="H":
-        pass
-
-    elif choice=="G":
-        pass
-
-    else:
-        print("Invalid message")
+def main():
     print(menu)
-    choice=input(">>>").upper()
-print("Fare well.")
+    choice = input(">>>").upper()
+    while choice != "Q":
+        if choice == "G":
+            user_score = get_valid_score()
+            print(f"Your score is {user_score}.")
+
+        elif choice == "P":
+            determine_score(user_score)
+            print(f"Your score {user_score} is {determine_score(user_score)}.")
+
+        elif choice == "S":
+            print("*" * user_score)
+
+        else:
+            print("Invalid message")
+        print(menu)
+        choice = input(">>>").upper()
+
+    print("Fare well.")
+
+
+def get_valid_score():
+    user_score=int(input("Score :"))
+    while user_score < 0 or user_score > 100:
+        print("Invalid Score")
+        user_score = int(input("Score :"))
+    return user_score
+
+def determine_score(user_score):
+    if user_score < 50:
+        return "Bad"
+    elif user_score > 90:
+        return "Excellent"
+    else:
+        return "Passable"
+
+
+
+main()
+
