7de413ae4535bc3e219f8347c773b5def7890609
christineyamin <shweyaminoo159932@gmail.com>
2024-02-25 16:03:29 +0800

    converting Celsius to Fahrenheit and vice versa.

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,18 +1,4 @@
 
-# def convert_celsius_to_Fahrennheit():
-#     celsius = float(input("Celsius: "))
-#     fahrenheit = celsius * 9.0 / 5 + 32
-#     print(f"Result: {fahrenheit:.2f} F")
-#
-# def convert_Fahrenheit_to_celsius():
-#     fahrenheit = float(input("Fahrenheit: "))
-#     celsius = 5 / 9 * (fahrenheit - 32)
-#     print(f"Result : {celsius:.2f} C")
-#
-# convert_celsius_to_Fahrennheit()
-# convert_Fahrenheit_to_celsius()
-
-
 def main():
     celsius = float(input("Celsius: "))
     convert_celsius_to_Fahrennheit(celsius)
