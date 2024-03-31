86a83190a50c6454ed1072fe40e09dcefb07ba17
christineyamin <shweyaminoo159932@gmail.com>
2024-03-31 15:58:15 +0800

    Complete dynamic_labels.kv and.py

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,35 @@
+"""
+Kivy example for CP1404/CP5632, IT@JCU
+Dynamically create buttons based on content of dictionary
+Lindsay Ward, first version: 11/07/2016
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+NEW_COLOUR = (1, 0, 0, 1)  # RGBA for red
+ALTERNATIVE_COLOUR = (1, 0, 1, 1)  # RGBA for magenta
+
+
+class DynamicWidgetsApp(App):
+    """Main program - Kivy app to demo dynamic widget creation."""
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        # basic data (model) example - dictionary of names: phone numbers
+        self.names = ["Bob Brown" , "Cat Cyan" ,"Oren Ochre"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Widgets"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        for name in self.names:
+            name_label = Label (text=name)
+            self.root.ids.main.add_widget(name_label)
+        return self.root
+
+DynamicWidgetsApp().run()
